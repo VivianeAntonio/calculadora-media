5c1164c16620b741844c16755df72f967f2c9319
Viviane Antonio <asilvaviviane@gmail.com>
2021-11-06 10:20:49 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var nome = "Viviane";
+var notaDoPrimeiroBimestre = 9;
+var notaDoSegundoBimestre = 7;
+var notaDoTerceiroBimestre = 4;
+var notaDoQuartoBimestre = 2;
+
+var notaFinal =
+  (notaDoPrimeiroBimestre +
+    notaDoSegundoBimestre +
+    notaDoTerceiroBimestre +
+    notaDoQuartoBimestre) /
+  4;
+
+var notaFixada = notaFinal.toFixed(1);
+
+console.log("Bem Vindo(a) " + nome);
+console.log(notaFixada);
+
+//Conceitos aprendidos
+//Variáveis, srings, console.log, toFixed, operações matemáticas, concatenação.
